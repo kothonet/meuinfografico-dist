f79504229d244e905e530604272ed4056d2440a1
Alessandro Gonçalves <20360165+kothonet@users.noreply.github.com>
2018-09-14 19:19:57 -0300

    initial commit

---
+++ precache-manifest.78feab1c17656c8c5f4cab64deadfaf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "50e14d7658070660ed73",
+    "url": "/js/chunk-vendors.3f528f40.js"
+  },
+  {
+    "revision": "b7206acd49a857e2e4b5",
+    "url": "/js/app.2ecb0dd9.js"
+  },
+  {
+    "revision": "ebe968f61be8c7c06f8059e221f01a8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50e14d7658070660ed73",
+    "url": "/css/chunk-vendors.0c3e5376.css"
+  },
+  {
+    "revision": "b7206acd49a857e2e4b5",
+    "url": "/css/app.89e603fa.css"
+  }
+];
